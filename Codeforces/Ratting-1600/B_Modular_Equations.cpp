0ac305220fe0a34804843f24470965d96b9372e6
Tanvir-136 <tanvir12islam34@gmail.com>
2023-12-22 20:56:35 +0600

    22/12/23

---
+++ Codeforces/Ratting-1600/B_Modular_Equations.cpp
@@ -0,0 +1,46 @@
+// B. Modular Equations
+// Author: Md.Tanvir Islam
+// Date:22-12-23
+
+//---------------------------------------------------------------//
+#include<bits/stdc++.h>
+#define FAST_IO ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define ll long long
+using namespace std;
+//---------------------------------------------------------------//
+void solve()
+{
+    int a,b;
+    cin>>a>>b;
+    if(a==b){
+        cout<<"infinity"<<'\n';
+    }else if(b>a){
+        cout<< 0 <<'\n';
+    }
+    else{
+        int cnt = 0;
+        a = a-b;
+        for (int i = 1; i <= (a + 1) / i; i++){
+            if (a % i == 0){
+                int x = a/i ;
+                if(i > b){
+                    cnt++;
+                }
+                if (x != i && x > b){
+                    cnt++;
+                }
+            }
+        }
+        cout<<cnt<<'\n';
+    }
+}
+int main(){
+    FAST_IO;
+    //Start Here
+    int t=1;
+    //cin >> t;
+    while (t--){
+        solve();
+    }
+    return 0;
+}
