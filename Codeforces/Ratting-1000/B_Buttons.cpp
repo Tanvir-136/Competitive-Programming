d5815e81b8e485ace8139929dba60bf8fc84693d
Tanvir-136 <tanvir12islam34@gmail.com>
2025-07-19 23:41:00 +0600

    update 800, 1000

---
+++ Codeforces/Ratting-1000/B_Buttons.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define FAST_IO ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define dbg(x) cout<<#x<<" = "<<x<<'\n';
+#define all(x) (x).begin(), (x).end()
+#define yes cout<<"YES"<<'\n';
+#define no cout<<"NO"<<'\n';
+#define ll long long
+#define MOD 1e9 + 7
+#define nl '\n'
+/*---------------------------------------------------------------*/
+const int N = 1e5 + 10;
+using namespace std;
+/*---------------------------------------------------------------*/
+void solve(){
+    ll n, ans = 0;
+    cin >> n;
+    for(ll i = 1; i < n; ++i){
+        ans += i * (n - i);
+    }
+    cout << ans + n << nl;
+}
+int main(){
+    FAST_IO;
+    //Start Here
+    int t=1;
+    //cin >> t;
+    while (t--){
+        solve();
+    }
+    return 0;
+}
